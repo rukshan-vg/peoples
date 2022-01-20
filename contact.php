2f18f493bee16691e179a3cf314f95c7ef1571b7
rukshan-vg <98074687+rukshan-vg@users.noreply.github.com>
2022-01-20 10:29:23 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+
+
+
+<body>
+    <!-- Header Section Start -->
+    <?php include 'include/header.php' ; ?>
+    <!-- Header Section End -->
+
+    <!-- Breadcrumb Section Begin -->
+    <div class="breadcrumb-option contact-breadcrumb set-bg" data-setbg="img/breadcrumb/contact-breadcrumb.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>Contact Us</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section End -->
+
+    <!-- Footer start -->
+    <?php include 'include/footer.php' ; ?>
+    <!-- Footer End -->
+</body>
+
+</html>
